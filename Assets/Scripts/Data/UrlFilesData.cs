0b37d1967943a0209bf5357277332b49fd187933
Valentin Nazarenko <dxoraxslife@gmail.com>
2025-01-21 16:56:04 +0400

    Added data scriptable object script

---
+++ Assets/Scripts/Data/UrlFilesData.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace MWTP.Data
+{
+    [CreateAssetMenu(fileName = "url", menuName = "Url Data", order = 0)]
+    public class UrlFilesData : ScriptableObject
+    {
+        [field:SerializeField] public string Settings { get; private set; }
+        [field:SerializeField] public string Texts { get; private set; }
+        [field:SerializeField] public string Sprite { get; private set; }
+    }
+}
