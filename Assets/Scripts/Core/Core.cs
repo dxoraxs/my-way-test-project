04890d4f49822d61cc698d9b1dab831d808b6b38
Valentin Nazarenko <dxoraxslife@gmail.com>
2025-01-21 19:29:52 +0400

    Update Core.cs

--- Assets/Scripts/Core/Core.cs
+++ Assets/Scripts/Core/Core.cs
@@ -11,40 +11,58 @@ public class Core : MonoBehaviour
 {
     [SerializeField] private UrlFilesData _urlFilesData;
     [SerializeField] private LoadScreenContainer _loadScreenContainer;
+    [SerializeField] private GameScreenContainer _gameScreenContainer;
     private IJsonLoader _jsonLoader;
     private IAssetBundleLoader _assetBundleLoader;
+    private SettingsConfig _settingsConfig;
+    private TextsConfig _textsConfig;
+    private Sprite _buttonBackgroundSprite;
 
     private void Start()
     {
         _jsonLoader = new JsonLoader(_urlFilesData.Settings, _urlFilesData.Texts);
         _assetBundleLoader = new AssetBundleLoader(_urlFilesData.Sprite);
 
-        LoadGameFiles();
+        InitializeGame();
     }
 
-    private async void LoadGameFiles()
+    private async void InitializeGame()
+    {
+        await LoadGameFiles();
+        InitializeGameScreen();
+    }
+
+    private async UniTask LoadGameFiles()
     {
         _loadScreenContainer.ShowScreen();
         
         var settingsJson = await _jsonLoader.LoadSettings();
-        var settings = LoadFilesConvertHelper.ConvertJsonToObject<SettingsConfig>(settingsJson);
+        _settingsConfig = LoadFilesConvertHelper.ConvertJsonToObject<SettingsConfig>(settingsJson);
         _loadScreenContainer.UpdateValue(1/3f);
         
         await UniTask.Delay(1000);
 
         var textsJson = await _jsonLoader.LoadTexts();
-        var texts = LoadFilesConvertHelper.ConvertJsonToObject<TextsConfig>(textsJson);
+        _textsConfig = LoadFilesConvertHelper.ConvertJsonToObject<TextsConfig>(textsJson);
         _loadScreenContainer.UpdateValue(2/3f);
         
         await UniTask.Delay(1000);
 
         _assetBundleLoader.ReleaseLoadedAsset();
         var spriteAssetBundle = await _assetBundleLoader.LoadSpriteAsset();
-        var sprite = spriteAssetBundle.GetSpriteFromAssetBundle();
+        _buttonBackgroundSprite = spriteAssetBundle.GetSpriteFromAssetBundle();
         _loadScreenContainer.UpdateValue(1);
 
         await UniTask.Delay(1000);
         
         _loadScreenContainer.HideScreen();
+    }
+
+    private void InitializeGameScreen()
+    {
+        _gameScreenContainer.ShowScreen();
+        _gameScreenContainer.SetWelcomeText(_textsConfig.welcomeMessage);
+        _gameScreenContainer.UpdateCounterText(_settingsConfig.startingNumber);
+        _gameScreenContainer.SetButtonBackgroundSprite(_buttonBackgroundSprite);
     }
 }
